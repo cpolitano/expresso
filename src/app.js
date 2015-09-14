d532703a7dda90e8afe738d12f3fee695fb49b1e
cpolitano <cpolitano8@gmail.com>
2015-09-14 08:25:16 -0400

    added env and debug modules

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('dotenv').load();
+
 var express = require('express'); 
 
 var app = express();
@@ -8,6 +10,11 @@ app.get('/', function(req, res){
   res.send('<h1>oh hai</h1>');
 });
 
+app.get('/blog', function(req, res){
+
+});
+
 app.listen(3000, function(){
   console.log('Frontend server running on port 3000');
+  // console.log(process.env.YELP_API_KEY);
 });
